f2d124ef7a6bcf0b1ff564b8e7d5127a9e02dc24
psp589 <pablosanchez589@gmail.com>
2016-04-22 10:10:36 +0200

    module

---
+++ src/Models/LayoutSelectorPart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+
+namespace Downplay.Orchard.LayoutSelector.Models
+{
+    public class LayoutSelectorPart : ContentPart<LayoutSelectorPartRecord>
+    {
+
+        public string LayoutName
+        {
+            get { return Record.LayoutName; }
+            set { Record.LayoutName = value; }
+        }
+
+        /// <summary>
+        /// This will be populated from the driver.
+        /// TODO: I saw an example of this in another module; is this Best Practice or is there a better way to provide this kind of data to the UI?
+        /// </summary>
+        public IEnumerable<String> AvailableLayouts { get; set; }
+
+    }
+}
---
+++ src/Services/LayoutSelectorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Downplay.Orchard.LayoutSelector.Services
+{
+    public class LayoutSelectorService : ILayoutSelectorService
+    {
+
+        private readonly IEnumerable<ILayoutAlternatesProvider> _providers;
+
+        public LayoutSelectorService(IEnumerable<ILayoutAlternatesProvider> providers)
+        {
+            _providers = providers;
+        }
+        
+        public IEnumerable<string> GetLayouts()
+        {
+            foreach (var p in _providers)
+            {
+                foreach (var l in p.GetLayouts())
+                {
+                    yield return l;
+                }
+            }
+        }
+    }
+}
